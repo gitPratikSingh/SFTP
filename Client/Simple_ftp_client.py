da28d6f12e49fcbe5b429e04bcacc88f8a7ed5a5
Pratik Singh <pratik.iem01@gmail.com>
2018-04-20 00:56:38 -0400

    Fixed remote server client issues

--- Client/Simple_ftp_client.py
+++ Client/Simple_ftp_client.py
@@ -16,7 +16,7 @@ ack_flag = '1010101010101010'
 end_flag = "1111111111111111"
 last_ack_num = -1
 client_socket = None
-server_hostname = 'localhost'
+server_hostname = '152.46.19.25'
 server_port = 7735
 lock = None
 
@@ -41,6 +41,7 @@ def start_client():
     client_name, client_port, file_name, go_back_N, MSS = sys.argv
     client_port = int(client_port)
     go_back_N = int(go_back_N)
+    client_socket.bind(('0.0.0.0', client_port))
 
     buffer_list = list()
     with open(file_name, "r") as file:  # with encapsulates common preparation and cleanup tasks
@@ -58,7 +59,6 @@ def start_client():
         thread_second.daemon = True
         thread_second.start()
         thread_second.join()
-        time.sleep(10)
     except KeyboardInterrupt:
         sys.exit(0)
 
@@ -70,21 +70,20 @@ def recv_ack(condition):
     global client_socket
     try:
         while 1:
-           # print(client_socket)
             ack_byte, addr = client_socket.recvfrom(1024)
             ack_num, zeros, ack_flag = pickle.loads(ack_byte)
-            print("received ack_num: " + str(ack_num))
+            #print("received ack_num: " + str(ack_num))
 
             if zeros == '0000000000000000' and ack_flag == ack_flag:
                 with condition:  # automatically call acquire at beginning and release at the end of block
                     if last_ack_num is None:
                         last_ack_num = ack_num
                         condition.notify()
-                        print("Updated ack")
+                        #print("Updated ack")
                     elif ack_num > last_ack_num:
                         last_ack_num = ack_num
                         condition.notify()
-                        print("Updated ack")
+                        #print("Updated ack")
     except KeyboardInterrupt:
         sys.exit(0)
 
@@ -97,7 +96,7 @@ def sendWindow(window):
     global server_hostname
     global server_port
     for win in window:
-       # print(client_socket)
+        #print(server_hostname)
         client_socket.sendto(win, (server_hostname, server_port))
 
 
@@ -118,7 +117,7 @@ def rdt_send(buffer_list, condition):
         # add data_flag
         with condition:
             before_ack_number = last_ack_num
-            print("Last ack val: "+str(last_ack_num))
+            #print("Last ack val: "+str(last_ack_num))
 
         if before_ack_number < max_ack:
             window = list()
@@ -135,16 +134,18 @@ def rdt_send(buffer_list, condition):
                 packet.append(buffer_list[last_packet_ack_number + 1])
 
                 last_packet_ack_number += 1
-                print("Sending " + str(last_packet_ack_number))
+                #print("Sending " + str(last_packet_ack_number))
                 packet = pickle.dumps(packet)
                # print(type(packet))
                 window.append(packet)
+
             sendWindow(window)
             with condition:
                 after_ack_number = last_ack_num
                 if after_ack_number == before_ack_number:
                     condition.wait(float(RTT))  # should wake up upon last_ack_num changes!
         else:
+
             #completed,send the end packet
             last_packet_ack_number = before_ack_number
             # implement lock
@@ -152,20 +153,20 @@ def rdt_send(buffer_list, condition):
             packet = list()
             header = list()
             header.append(last_packet_ack_number + 1)
-            header.append(checksum(data_flag))
+            header.append(checksum(end_flag))
             header.append(end_flag)
             packet.append(header)
             packet.append(end_flag)
 
             last_packet_ack_number += 1
-            print("Sending end packet" + str(last_packet_ack_number))
+            #print("Sending end packet" + str(last_packet_ack_number))
             packet = pickle.dumps(packet)
 
             window.append(packet)
-            sendWindow(window, client_socket, server_hostname, server_port)
-            break
+            sendWindow(window)
+            break;
+
 
 
 if __name__ == "__main__":
     start_client()
-    print('Client is ready to go!')
